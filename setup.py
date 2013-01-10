441ccb4bce05db0e6d7788c1d819632b6dcf3d8f
Andy Stewart <lazycat.manatee@gmail.com>
2013-01-10 08:47:47 +0700

    Add blur and cookie modules from deepin-ui.

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 #! /usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, Extension
+import commands
+
+def pkg_config_cflags(pkgs):
+    '''List all include paths that output by `pkg-config --cflags pkgs`'''
+    return map(lambda path: path[2::], commands.getoutput('pkg-config --cflags-only-I %s' % (' '.join(pkgs))).split())
+
+cairo_mod = Extension('dtk_cairo_blur',
+                include_dirs = pkg_config_cflags(['cairo', 'pygobject-2.0']),
+                libraries = ['cairo', 'pthread', 'glib-2.0'],
+                sources = ['./deepin_utils/cairo_blur.c'])
+webkit_mod = Extension('dtk_webkit_cookie',
+                include_dirs = pkg_config_cflags(['gtk+-2.0', 'webkit-1.0', 'pygobject-2.0']),
+                libraries = ['webkitgtk-1.0', 'soup-2.4', 'pthread', 'glib-2.0'],
+                sources = ['./deepin_utils/webkit_cookie.c'])
 
 setup(name='deepin_utils',
       version='1.0',
+      ext_modules = [cairo_mod, webkit_mod],
       description='Basic utils for all projects in Linux Deepin.',
       long_description ="""Python download library for Linux DeepinBasic utils for all projects in Linux Deepin.""",
       author='Wang Yong',
