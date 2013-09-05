e60c8431dad1d3495977ece1c9fcb46a5f69eb46
Andy Stewart <lazycat.manatee@gmail.com>
2013-09-05 11:19:25 +0800

    Add font module.

---
+++ deepin_utils/font.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 ~ 2013 Deepin, Inc.
+#               2011 ~ 2013 Wang Yong
+# 
+# Author:     Wang Yong <lazycat.manatee@gmail.com>
+# Maintainer: Wang Yong <lazycat.manatee@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pangocairo
+
+def get_font_families():
+    '''Get all font families in system.'''
+    fontmap = pangocairo.cairo_font_map_get_default()
+    return sorted(map(lambda f: f.get_name(), fontmap.list_families()))
+
